0b74b25eed88d55831445e33b8a541165b75aae6
muhammadnurfuadi <mhmmdfuaad12@gmail.com>
2023-06-17 19:37:48 +0700

    revisi menu

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
-        <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid">
-                        <div class="d-flex align-items-center justify-content-between small"> 
-                            <div class="text-muted">Copyright &copy; Your Website 2021</div> 
+<footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
@@ -12,8 +12,7 @@
                 </footer>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" cross origin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script> 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?= base_url('assets/js/scripts.js')?>"></script>
-</body>
+    </body>
 </html>
